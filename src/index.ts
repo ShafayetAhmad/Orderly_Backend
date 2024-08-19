eea26bf9e658f8c7857116f80308e1a5cdbda077
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2024-08-19 15:56:48 +0600

    save product api created

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
-import { Request, Response } from "express";
+import { Application, Request, Response } from "express";
 
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import productsRouter from "./Product.routes";
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app: Application = express();
+app.use(express.json());
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+  } catch (error) {
+    console.log("MongoDB Error: ", error);
+    process.exit(1);
+  }
+}
+
+app.use("/api/products", productsRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
