7c110c3f2b4bd825aefab473c61a2178c6ab3fe1
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2024-08-19 15:44:02 +0600

    product interface created

---
+++ src/Product.interface.ts
@@ -0,0 +1,17 @@
+interface Product extends Document {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: {
+    type: string;
+    value: string;
+  }[];
+  inventory: {
+    quantity: number;
+    inStock: boolean;
+  };
+}
+
+export default Product;
